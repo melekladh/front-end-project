903d4d3c5c9404b3acbff54fd8e124aaa44149d1
melekladh <ladharimalek10@gmail.com>
2024-04-26 13:19:33 +0100

    login

--- src/compenents/Register.js
+++ src/compenents/Register.js
@@ -1,79 +1,20 @@
-import React, { useRef } from 'react'
-import { UserRegister } from '../redux/slices/userSlice'
-import { useDispatch, useSelector } from 'react-redux'
-// import "../css/register.css"
-import '../css/product.css'
-const Register = () => {
-  const email=useRef()
-  const password=useRef()
-const dispatch=useDispatch()
-const {error}=useSelector(state=>state.user)
-  return (
-
-    <div>
-      
-
-
-
-      {/* <div className="container">
-	<div className="screen">
-		<div className="screen__content">
-			<form className="login">
-				<div className="login__field">
-					<i className="login__icon fas fa-user"></i>
-					<input type="text" class="login__input" placeholder="User name / Email"></input>
-				</div>
-				<div className="login__field">
-					<i className="login__icon fas fa-lock"></i>
-					<input type="password" class="login__input" placeholder="Password"></input>
-				</div>
-				<button className="button login__submit">
-					<span className="button__text">Log In Now</span>
-					<i className="button__icon fas fa-chevron-right"></i>
-				</button>				
-			</form>
-			<div className="social-login">
-				<h3>log in via</h3>
-				<div className="social-icons">
-					<a href="#" className="social-login__icon fab fa-instagram"></a>
-					<a href="#" className="social-login__icon fab fa-facebook"></a>
-					<a href="#" className="social-login__icon fab fa-twitter"></a>
-				</div>
+<div id="registration-form">
+	<div class='fieldset'>
+    <legend>Wanna be Cool?!</legend>
+		<form action="#" method="post" data-validate="parsley">
+			<div class='row'>
+				<label for='firstname'>First Name</label>
+				<input type="text" placeholder="First Name" name='firstname' id='firstname' data-required="true" data-error-message="Your First Name is required"></input>
 			</div>
-		</div>
-		<div className="screen__background">
-			<span className="screen__background__shape screen__background__shape4"></span>
-			<span className="screen__background__shape screen__background__shape3"></span>		
-			<span className="screen__background__shape screen__background__shape2"></span>
-			<span className="screen__background__shape screen__background__shape1"></span>
-		</div>		
+			<div class='row'>
+				<label for="email">E-mail</label>
+				<input type="text" placeholder="E-mail"  name='email' data-required="true" data-type="email" data-error-message="Your E-mail is required"></input>
+			</div>
+			<div class='row'>
+				<label for="cemail">Confirm your E-mail</label>
+				<input type="text" placeholder="Confirm your E-mail" name='cemail' data-required="true" data-error-message="Your E-mail must correspond"></input>
+			</div>
+			<input type="submit" value="Register"></input>
+		</form>
 	</div>
-</div> */}
-
-     {/* <div>
-     <input type='email' placeholder='type your email' ref={email}></input>
-   <input type='password' placeholder='type your password'ref={password}></input>
-     <button onClick={()=>dispatch(UserRegister({
-       email:email.current.value,
-       password:password.current.value
-        }))}>register</button>
-     {error&& <h1>{error}</h1>}
-   
-
-   </div> */}
-
-<div className="login-box"><h2>Login</h2><form><div className="user-box">
-        <input type="text" name=""></input>
-            <label>Username</label>
-        </div><div className="user-box">
-            <input type="password" name=""></input>
-            <label>Password</label></div><a href="#"><span></span><span></span><span></span>
-            <span></span>Submit</a></form></div>
-
-
-
-    </div>
-  )
-}
-
-export default Register
+</div>

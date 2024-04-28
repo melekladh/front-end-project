d89348baa50ba4f4a2f9f677987c246cac457869
melekladh <ladharimalek10@gmail.com>
2024-04-28 19:21:15 +0100

    done

--- src/compenents/Register.js
+++ src/compenents/Register.js
@@ -1,24 +1,99 @@
-import React, { useRef } from 'react'
-import { UserRegister } from '../redux/slices/userSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import "../css/register.css"
-const Register = () => {
-  const email=useRef()
-  const password=useRef()
-const dispatch=useDispatch()
-const {error}=useSelector(state=>state.user)
-  return (
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { UserRegister } from '../redux/slices/userSlice';
+import "../css/register.css";
+import "../fonts/material-icon/css/material-design-iconic-font.min.css";
+import "../css/style.css";
 
-     <div className='input-form' style={{width:'20%',display:'flex',flexDirection:'column',alignItems:'center',gap:'10px',color:'blue'}}>
-     <input type='email' placeholder='type your email' ref={email}></input>
-   <input type='password' placeholder='type your password'ref={password}></input>
-     <button onClick={()=>dispatch(UserRegister({
-       email:email.current.value,
-       password:password.current.value
-        }))}>register</button>
-     {error&& <h1>{error}</h1>}
-   </div>
-  )
+const Register = () => {
+  const email = useRef();
+  const password = useRef();
+  const dispatch = useDispatch();
+  const { error } = useSelector(state => state.user);
+
+  return (
+    <div className="main">
+      {/* Sign up form */}
+      <section className="signup">
+        <div className="container">
+          <div className="signup-content">
+            <div className="signup-form">
+              <h2 className="form-title">Sign up</h2>
+              <form method="POST" className="register-form" id="register-form">
+                <div className="form-group">
+                  <label htmlFor="name"><i className="zmdi zmdi-account material-icons-name"></i></label>
+                  <input type="text" name="name" id="name" placeholder="Your Name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email"><i className="zmdi zmdi-email"></i></label>
+                  <input type="email" name="email" id="email" placeholder="Your Email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="pass"><i className="zmdi zmdi-lock"></i></label>
+                  <input type="password" name="pass" id="pass" placeholder="Password" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="re-pass"><i className="zmdi zmdi-lock-outline"></i></label>
+                  <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password" />
+                </div>
+                <div className="form-group">
+                  <input type="checkbox" name="agree-term" id="agree-term" className="agree-term" />
+                  <label htmlFor="agree-term" className="label-agree-term"><span><span></span></span>I agree all statements in <a href="#" className="term-service">Terms of service</a></label>
+                </div>
+                <div className="form-group form-button">
+                  <input type="submit" name="signup" id="signup" className="form-submit" value="Register" />
+                </div>
+              </form>
+            </div>
+            <div className="signup-image">
+              <figure><img src="images/signup-image.jpg" alt="sign up" /></figure>
+              <a href="#" className="signup-image-link">I am already a member</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Sign in Form */}
+      <section className="sign-in">
+        <div className="container">
+          <div className="signin-content">
+            <div className="signin-image">
+              <figure><img src="images/signin-image.jpg" alt="sign in" /></figure>
+              <a href="#" className="signup-image-link">Create an account</a>
+            </div>
+            <div className="signin-form">
+              <h2 className="form-title">Sign in</h2>
+              <form method="POST" className="register-form" id="login-form">
+                <div className="form-group">
+                  <label htmlFor="your_name"><i className="zmdi zmdi-account material-icons-name"></i></label>
+                  <input type="text" name="your_name" id="your_name" placeholder="Your Name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="your_pass"><i className="zmdi zmdi-lock"></i></label>
+                  <input type="password" name="your_pass" id="your_pass" placeholder="Password" />
+                </div>
+                <div className="form-group">
+                  <input type="checkbox" name="remember-me" id="remember-me" className="agree-term" />
+                  <label htmlFor="remember-me" className="label-agree-term"><span><span></span></span>Remember me</label>
+                </div>
+                <div className="form-group form-button">
+                  <input type="submit" name="signin" id="signin" className="form-submit" value="Log in" />
+                </div>
+              </form>
+              <div className="social-login">
+                <span className="social-label">Or login with</span>
+                <ul className="socials">
+                  <li><a href="#"><i className="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                  <li><a href="#"><i className="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                  <li><a href="#"><i className="display-flex-center zmdi zmdi-google"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Register
+export default Register;

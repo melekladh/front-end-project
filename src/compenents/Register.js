0eaa3946c48f35ea64b5cfdb4cf455b62d32952b
melekladh <ladharimalek10@gmail.com>
2024-04-28 17:08:42 +0100

    width  form to 10%

--- src/compenents/Register.js
+++ src/compenents/Register.js
@@ -12,19 +12,20 @@ const {error}=useSelector(state=>state.user)
     <div>
       
 
-<div id="registration-form">
-	<div class='fieldset'>
+<div id="registration-form" >
+	<div className='fieldset'>
     <legend>Wanna be Cool?!</legend>
 		<form action="#" method="post" data-validate="parsley">
-			<div class='row'>
+			
+			<div className='row'>
 				<label for='firstname'>First Name</label>
 				<input type="text" placeholder="First Name" name='firstname' id='firstname' data-required="true" data-error-message="Your First Name is required"></input>
 			</div>
-			<div class='row'>
+			<div className='row'>
 				<label for="email">E-mail</label>
 				<input type="text" placeholder="E-mail"  name='email' data-required="true" data-type="email" data-error-message="Your E-mail is required"></input>
 			</div>
-			<div class='row'>
+			<div className='row'>
 				<label for="cemail">Confirm your E-mail</label>
 				<input type="text" placeholder="Confirm your E-mail" name='cemail' data-required="true" data-error-message="Your E-mail must correspond"></input>
 			</div>
